66af677fbfdf0c67efcf786e7558ed636750070d
Liew Jun Young <liewjunyoung@gmail.com>
2023-08-02 16:16:20 +0800

    xray with napp

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -4,41 +4,50 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Napp\Xray\Facades\Xray;
+// use Napp\Xray\Facades\Xray;
 
 class UploadController extends Controller
 {
     public function upload(Request $request)
     {
+        
+        Xray::addSegment('MyCustomLogic');
+
         $request->validate([
             'file' => 'required|mimes:jpeg,png,pdf|max:2048', // Adjust the allowed file types and size as needed
         ]);
-    
+
         if ($request->file('file')->isValid()) {
             $file = $request->file('file');
-            $fileName = $file-> getClientOriginalName();
-    
+            $fileName = $file->getClientOriginalName();
+
             // Get the path of the uploaded file
             $sourceFilePath = $file->getRealPath();
-    
+
+
             // Create S3 client
             $s3 = \App::make('aws')->createClient('s3');
-    
+
             try {
                 $result = $s3->putObject([
-                    'Bucket'     => env('AWS_BUCKET'),
-                    'Key'        => $fileName,
+                    'Bucket' => env('AWS_BUCKET'),
+                    'Key' => $fileName,
                     'SourceFile' => $sourceFilePath,
                 ]);
             } catch (\Aws\S3\Exception\S3Exception $e) {
                 // Catch any S3 exceptions and return an error message
                 return redirect()->back()->with('error', 'Failed to upload the file to S3: ' . $e->getMessage());
             }
-    
+
             $url = $result['ObjectURL'];
-    
+            // run your code
+
+              Xray::endSegment('MyCustomLogic');
+
             return redirect()->back()->with('success', 'File uploaded successfully. File path: ' . $url);
         }
-    
+
         return redirect()->back()->with('error', 'Failed to upload the file.');
     }
 }
---
+++ config/xray.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'enabled' => env('XRAY_ENABLED', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Submission method
+    |--------------------------------------------------------------------------
+    |
+    | This is where you can set the data submission method.
+    | Supported classes: "APISegmentSubmitter", "DaemonSegmentSubmitter"
+    |
+    */
+     'submitter' => \Napp\Xray\Submission\APISegmentSubmitter::class,
+   // 'submitter' => \Napp\Xray\Submission\DaemonSegmentSubmitter::class,
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | User resolver method
+    |--------------------------------------------------------------------------
+    |
+    | Here you can set a class to find the logged-in user identifier.
+    | Supported classes: "AuthIdentifier"
+    |
+    */
+    'user-resolver' => \Napp\Xray\Resolvers\AuthIdentifier::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Database Query
+    |--------------------------------------------------------------------------
+    */
+    'db_query' => env('XRAY_DB_QUERY', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Database Query Bindings
+    |--------------------------------------------------------------------------
+    */
+    'db_bindings' => env('XRAY_DB_QUERY_BINDINGS', false),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trace Queue Jobs
+    |--------------------------------------------------------------------------
+    */
+    'job' => env('XRAY_JOB', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trace Views
+    |--------------------------------------------------------------------------
+    */
+    'view' => env('XRAY_VIEW', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trace Routes
+    |--------------------------------------------------------------------------
+    */
+    'route' => env('XRAY_ROUTE', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trace Routes
+    |--------------------------------------------------------------------------
+    */
+    'framework' => env('XRAY_FRAMEWORK', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trace Cache
+    |--------------------------------------------------------------------------
+    */
+    'cache' => env('XRAY_CACHE', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Use LAMBDA_INVOCATION_CONTEXT
+    |--------------------------------------------------------------------------
+    */
+    'use_lambda_invocation_context' => env('XRAY_USE_LAMBDA_INVOCATION_CONTEXT', false),
+
+    /*
+    |--------------------------------------------------------------------------
+    | AWS, only needed if "APISegmentSubmitter" submitter is chosen
+    |--------------------------------------------------------------------------
+    */
+    'aws' => [
+        'region' => env('XRAY_AWS_REGION') ?? env('AWS_DEFAULT_REGION'),
+        'version' => env('XRAY_AWS_VERSION', 'latest'),
+        'signature_version' => env('XRAY_AWS_SIGNATURE_VERSION', 'v4'),
+        'credentials' => [
+            'key' => env('XRAY_AWS_ACCESS_KEY_ID') ?? env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('XRAY_AWS_SECRET_ACCESS_KEY') ?? env('AWS_SECRET_ACCESS_KEY'),
+            'token' => env('XRAY_AWS_TOKEN') ?? env('AWS_SESSION_TOKEN'),
+            'expires' => '',
+        ],
+    ],
+];
